79ff850db81d65db1465318a83f8c17405601fed
Noah-Amar <ne.amar@outlook.com>
2018-09-16 13:58:54 -0500

    last style

--- pupperiq/src/Header/Header.jsx
+++ pupperiq/src/Header/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
         <div>
             <h1> Welcome to PupperIQ </h1>
             <br/>
+            <img className='image' src='https://cdn.northstarmoving.com/blog/wp-content/uploads/2014/06/dog-beach-1024x500.jpg' />
             <h3> Take the quiz below to find your perfect pupper </h3>
         </div>
      );
---
+++ pupperiq/src/Header/Jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button, Icon, Carousel} from 'react-materialize'
+
+const Jumbotron = () => {
+    return (
+        <div className='image'>
+            
+        </div>
+      );
+}
+ 
+export default Jumbotron;
